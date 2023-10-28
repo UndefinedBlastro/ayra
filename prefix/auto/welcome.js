9b2caf12246aebafaea4456dc79f21d7a5758ad8
UndefinedBlastro <149256010+UndefinedBlastro@users.noreply.github.com>
2023-10-28 22:38:52 +0530

    Created welcome.js
    Added welcome embed

---
+++ prefix/auto/welcome.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: "",
+    type: "guildMemberAdd",
+    code:`  $sendMessage[$channelID[✉︱general];$title[Welcome to Ayra Cafe $username <a:exclamation_mark:1160909279788810322>] $description[
+        . Chat with us!
+        . Boost us!
+        . Check out [gws\\](https://discordapp.com/channels/982931539941818408/1000422092300353636) !!]
+        $footer[$username;$userAvatar[$authorID]] $color[#c1baff] $image[https://i.imgur.com/GUEUhHv.gif
+]  Welcome, <@$authorID> ! ]`,
+    unprefixed: false
+
+}
