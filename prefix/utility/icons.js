027b3c9ad80a3492b404ee39892cba2505edcd47
UndefinedBlastro <149256010+UndefinedBlastro@users.noreply.github.com>
2023-11-06 19:21:44 +0530

    Create icons.js
    request icons/banner

---
+++ prefix/utility/icons.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: "req",
+    type: "messageCreate",
+    code: `
+    $sendMessage[1153979847035596820;$title[<a:exclamation_mark:1160909279788810322> New Request <a:exclamation_mark:1160909279788810322>] $description[$userDisplayName has requested for $message] $footer[Thank You!!] $color[#c1baff]<@&1088051165217968168> New Request by $userDisplayName <a:exclamation_mark:1160909279788810322> ]
+    `
+ }
