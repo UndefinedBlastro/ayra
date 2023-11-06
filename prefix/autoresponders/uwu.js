70d5da8aa3cee36bc1671a8154d8657050e40488
UndefinedBlastro <149256010+UndefinedBlastro@users.noreply.github.com>
2023-11-06 19:56:48 +0530

    Create uwu.js

---
+++ prefix/autoresponders/uwu.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: "uwu",
+    aliases: ["UwU", "Uwu", "Uwu", "uwu", "UWu", "UWU", "uWu", "uWU", "uwU"],
+    type: "messageCreate",
+    code:`E-girl Moment`,
+    unprefixed: true
+
+}
