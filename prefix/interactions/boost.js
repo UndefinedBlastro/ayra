c117b243472dabf141c68912816b51084fd8f462
UndefinedBlastro <149256010+UndefinedBlastro@users.noreply.github.com>
2023-11-06 20:47:36 +0530

    Create boost.js

---
+++ prefix/interactions/boost.js
@@ -0,0 +1,25 @@
+module.exports = {
+    type: "interactionCreate",
+    code: `$if[$customID==level2;$ephemeral $author[Level and Perks]$footer[ Ayra Cafe | $timestamp] $addField[Single Boost;<:heartpurple:1129304889659502655> : Custom role of your choice, including a role icon and your own colour!
+<:heartpurple:1129304889659502655> : Instant picture/image permissions in all channels.
+<:heartpurple:1129304889659502655> : Link permissions (you must follow our content rules).
+<:heartpurple:1129304889659502655> : Ability to add reactions to messages.
+<:heartpurple:1129304889659502655> : Roled with our exclusive and hoisted booster role: <@&990294744162762763>.
+<:heartpurple:1129304889659502655> : 3x giveaways entries and requirement bypass
+<:heartpurple:1129304889659502655> : 50 super cookies
+<:heartpurple:1129304889659502655> : One custom command
+<:heartpurple:1129304889659502655> : Ability to add two emojis
+]
+$addField[Double Boost;<:heartpurple:1129304889659502655> : 2 Custom role of your choice, including a role icon and your own colour!(1 is giftable)
+<:heartpurple:1129304889659502655> : Instant picture/image permissions in all channels.
+<:heartpurple:1129304889659502655> : Link permissions (you must follow our content rules).
+<:heartpurple:1129304889659502655> : Ability to add reactions to messages.
+<:heartpurple:1129304889659502655> : Roled with our exclusive and hoisted booster role: <@&990294744162762763>.
+<:heartpurple:1129304889659502655> : 4x giveaways entries and requirement bypass
+<:heartpurple:1129304889659502655> : 100 super cookies
+<:heartpurple:1129304889659502655> : One custom command
+<:heartpurple:1129304889659502655> : Ability to add four emojis]
+$addField[Notice;
+Please contact a member of our staff team to claim your perks! We cannot guarantee your perks if you do not reach out to us to request them, as we do not keep track of boosts to the server. All perks are not permanent and will be removed once you are no longer boosting us.]
+$title[ <a:nitroboost:1160909390048661564> Boosting Perks]$color[#9fd1ff]]`
+    }
