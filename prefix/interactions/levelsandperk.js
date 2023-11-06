add674332b16398328229b47c86543d8bd5b9337
UndefinedBlastro <149256010+UndefinedBlastro@users.noreply.github.com>
2023-11-06 19:42:38 +0530

    Update levelsandperk.js

--- prefix/interactions/levelsandperk.js
+++ prefix/interactions/levelsandperk.js
@@ -1,7 +1,7 @@
 module.exports = {
     type: "interactionCreate",
-    code: `$if[$customID==test2;$ephemeral[Click on the buttons below to check each Level and Booster Perks
+    code: `$if[$customID==test2;$ephemeral Click on the buttons below to check each Level and Booster Perks
 $addActionRow
     $addButton[level1;Level Perks;Primary;<:1154395376242139186:1164951117566853140>]
-    $addButton[level2;Booster Perks;Secondary;<a:nitroboost:1160909390048661564> ]]]`
+    $addButton[level2;Booster Perks;Secondary;<a:nitroboost:1160909390048661564> ]]`
     }
