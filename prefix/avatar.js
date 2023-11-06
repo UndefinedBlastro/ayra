7d5bde337dd1bbbb56261897a0277558c6dc937f
UndefinedBlastro <149256010+UndefinedBlastro@users.noreply.github.com>
2023-11-06 19:43:00 +0530

    Update avatar.js

--- prefix/avatar.js
+++ prefix/avatar.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "avatar",
     type: "messageCreate",
     code: ` $sendMessage[$channelID;$title[> $userDisplayName[$mentioned[0;true]]'s' Avatar] $image[$userAvatar[$mentioned[0;true]]
-] $footer[🖼 Avatar Fetched] $color[#c1baff]$author[🖼 Avatar Tool]$getTimestamp
+] $footer[🖼 Avatar Fetched | $Timestamp] $color[#c1baff]$author[🖼 Avatar Tool]
 $addActionRow
     $addButton[$userAvatar[$mentioned[0;true]];Avatar Link;Link]]`
 }
