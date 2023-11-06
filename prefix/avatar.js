8509b362a4170ec93e7a03fd20b1176f8076ebd4
UndefinedBlastro <149256010+UndefinedBlastro@users.noreply.github.com>
2023-11-06 16:45:20 +0530

    Update avatar.js
    changed the avatar embed

--- prefix/avatar.js
+++ prefix/avatar.js
@@ -1,6 +1,8 @@
 module.exports = {
     name: "avatar",
     type: "messageCreate",
-    code: ` $sendMessage[$channelID;$title[User Avatar] $image[$userAvatar[$mentioned[0;true]]
-] $footer[$username;$userAvatar[$authorID]] $color[#c1baff]]`
+    code: ` $sendMessage[$channelID;$title[> $userDisplayName[$mentioned[0;true]]'s' Avatar] $image[$userAvatar[$mentioned[0;true]]
+] $footer[🖼 Avatar Fetched] $color[#c1baff]$author[🖼 Avatar Tool]$getTimestamp
+$addActionRow
+    $addButton[$userAvatar[$mentioned[0;true]];Avatar Link;Link]]`
 }
