b72f5f19a863d4fd68bdc8512789c228f2ce3429
UndefinedBlastro <149256010+UndefinedBlastro@users.noreply.github.com>
2023-11-06 20:05:44 +0530

    Create level.js

---
+++ prefix/interactions/level.js
@@ -0,0 +1,26 @@
+module.exports = {
+    type: "interactionCreate",
+    code: `$if[$customID==level1;$ephemeral $author[Level and Perks]$footer[ Ayra Cafe | $timestamp]$description[<:heartpurple:1129304889659502655> <@&1033687674952876062>    Unlocks the ability to stream in voice chats.
+
+<:heartpurple:1129304889659502655> <@&984333155966283817> : Unlocks Embed Links and voice activities.
+<:heartpurple:1129304889659502655> <@&984333148420730950> : Unlocks  external stickers and use of soundboard.
+
+<:heartpurple:1129304889659502655> <@&985519090217152553>  : Increased chance getting accepted when applying for Staff, Attach files, Change nicknames, external soundboard.
+
+<:heartpurple:1129304889659502655> <@&984333118280437820> :  Send voice messages.
+
+<:heartpurple:1129304889659502655> <@&1130440335642857503> :  Unlocks the ability to start threads.
+<:heartpurple:1129304889659502655> <@&1130438481366224997>
+<:heartpurple:1129304889659502655> <@&1130438491533226034>
+
+<:heartpurple:1129304889659502655> <@&984952362210824235>
+
+<:heartpurple:1129304889659502655> <@&1130438476207239178>
+
+<:heartpurple:1129304889659502655> <@&1023135020744970240>
+
+<:heartpurple:1129304889659502655> <@&984952352962379777>
+
+<:heartpurple:1129304889659502655> <@&1130441649001078815>]
+$title[ <:1154395376242139186:1164951117566853140> Levelling Perks]]`
+    }
