1a708eed2e942b8c411833257884177335423bba
UndefinedBlastro <149256010+UndefinedBlastro@users.noreply.github.com>
2023-11-11 17:36:33 +0530

    Update starboard.js

--- prefix/auto/starboard.js
+++ prefix/auto/starboard.js
@@ -2,5 +2,5 @@ module.exports = {
 name: "customboard",
 type: "messageReactionAdd",
 code:`$onlyIf[$reactionEmoji==$getVar[staremoji;$guildID];]
-$onlyIf[$getMessageReactionCount[$channelID;$reactionMessageID;$getVar[staremoji;$guildID]]>=1;]$let[msg;$getMessage[$channelID;$reactionMessageID;content]]$sendMessage[$getVar[starchannel;$guildID];$reactionEmoji $getMessageReactionCount[$channelID;$reactionMessageID;$getVar[staremoji;$guildID]] <#$channelID> $color[Random]$addActionRow$addButton[https://discord.com/channels/$guildID/$channelID/$reactionMessageID;MSG-LINK;Link;$getVar[staremoji;$guildID]]$author[$username[$getMessage[$channelID;$reactionMessageID;authorID]];$userAvatar[$getMessage[$channelID;$messageID;authorID]]]$description[$if[$get[msg]==;Error:Cannot Access This Message!;$get[msg]]]]`
+$onlyIf[$getMessageReactionCount[$channelID;$reactionMessageID;$getVar[staremoji;$guildID]]>=1;]$sendMessage[$getVar[starchannel;$guildID];$title[[$channelName[$channelID]\\](https://discord.com/channels/$guildID/$channelID/$reactionMessageID) $color[Random]$description[$getMessage[$channelID;$reactionMessageID;content]]$author[$username[$getMessage[$channelID;$reactionMessageID;authorID]]]$thumbnail[$userAvatar[$getMessage[$channelID;$reactionMessageID;authorID]]]$footer[Ayra Star-Board]`
 }
