95557327e16a021f89148084a6bcf71582a81cb9
UndefinedBlastro <149256010+UndefinedBlastro@users.noreply.github.com>
2023-11-16 09:32:54 +0530

    Update confess.js

--- prefix/auto/confess.js
+++ prefix/auto/confess.js
@@ -1,20 +1,8 @@
 module.exports = {
     name: "",
     code: `
-$onlyIf[$and[$channelID==1113451860800569465;$messageType==GuildBoost]]
-$sendMessage[1113451860800569465;
-    $title[Thank you for boosting $userDisplayName]
-    $description[
-            . Thank you for boosting <@$authorID> ! <a:1005167440738660584:1163121131318947880>
-            . we are now at $guildBoostCount boosts <a:1160635816385708093:1163122257124659330>
-            . Check out our [perks\\](https://discordapp.com/channels/982931539941818408/983978053975818280) here  ‹𝟹
-    ]
-    $footer[$username;$userAvatar[$authorID]]
-    $color[#c1baff]
-    $image[https://i.imgur.com/GUEUhHv.gif]
-	 $timestamp
-    Thank you <@$authorID> !!
-
+$onlyIf[$channelID==1113451860800569465]
+$sendMessage[$channelID[₪︱confessions];$title[Anonymous Confession] $description[$message] $footer[❗ If this confession is ToS-breaking or overtly hateful, you can report it to staff] $color[Random]] $sendMessage[1073175125538643988;$title[$username[$authorID] Confession] $description[$message]]]
 ]`,
     type: "messageCreate",
     unprefixed: true
