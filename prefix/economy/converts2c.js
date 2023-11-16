4683a2ea496f8b4037e4abd5afc63f8113047f78
UndefinedBlastro <149256010+UndefinedBlastro@users.noreply.github.com>
2023-11-16 10:01:40 +0530

    Update converts2c.js

--- prefix/economy/converts2c.js
+++ prefix/economy/converts2c.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "converts2c",
     type: "messageCreate",
     code:`$if[$getVar[supercookie;$authorID;0]>=1;$setVar[supercookie;$authorID;$sub[$getVar[supercookie;$authorID];1]]
-$setVar[cookie;$authorID;$sum[100;$getVar[cookie;$authorID]]];Need atleast 1 supercookie to convert]`,
+$setVar[cookie;$authorID;$sum[200;$getVar[cookie;$authorID]]];Need atleast 1 supercookie to convert]`,
     unprefixed: true
 
 }
