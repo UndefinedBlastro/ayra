baf4c0bd4fdc133069f635cafb7a2b86ffcfd333
UndefinedBlastro <149256010+UndefinedBlastro@users.noreply.github.com>
2023-11-16 09:14:42 +0530

    Update banner.js

--- prefix/banner.js
+++ prefix/banner.js
@@ -1,6 +1,10 @@
 module.exports = {
     name: "banner",
     type: "messageCreate",
-    code: ` $sendMessage[$channelID;$title[User Banner] $image[$userBanner[$mentioned[0;true]]
-] $footer[$username;$userAvatar[$authorID]] $color[#c1baff]]`
+    code: ` $if[$userBanner[$mentioned[0;true]]==;$sendMessage[$channelID;$title[User Banner] $description[No banner found]
+
+$footer[🖼 Banner Fetched | $timestamp] $color[#c1baff]$author[🖼 Banner Tool]];$sendMessage[$channelID;$title[User Banner] $image[$userBanner[$mentioned[0;true]]]
+ $footer[🖼 Banner Fetched | $timestamp] $color[#c1baff]$author[🖼 Banner Tool]
+$addActionRow
+    $addButton[$userBanner[$mentioned[0;true]];Avatar Link;Link]]]`
 }
