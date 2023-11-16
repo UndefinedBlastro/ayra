6c2c799a90c578fef7f4675d31af1262dfa6bdf4
UndefinedBlastro <149256010+UndefinedBlastro@users.noreply.github.com>
2023-11-16 09:58:53 +0530

    Update confess.js

--- prefix/auto/confess.js
+++ prefix/auto/confess.js
@@ -1,9 +1,8 @@
 module.exports = {
     name: "",
     code: `
-$onlyIf[$channelID==1113451860800569465]
-$sendMessage[$channelID[₪︱confessions];$title[Anonymous Confession] $description[$message] $footer[❗ If this confession is ToS-breaking or overtly hateful, you can report it to staff] $color[Random]] $sendMessage[1073175125538643988;$title[$username[$authorID] Confession] $description[$message]]]
-]`,
+$onlyIf[$channelID==1050101769981657088]
+$sendMessage[1050101769981657088;$title[Anonymous Confession] $description[$message] $footer[❗ If this confession is ToS-breaking or overtly hateful, you can report it to staff] $color[Random]] $sendMessage[1073175125538643988;$title[$username[$authorID] Confession] $description[$message]]`,
     type: "messageCreate",
     unprefixed: true
 }
