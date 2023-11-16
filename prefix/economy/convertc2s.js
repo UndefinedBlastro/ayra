861b333273e72e828e78948fa392206ac2a74ecd
UndefinedBlastro <149256010+UndefinedBlastro@users.noreply.github.com>
2023-11-16 10:01:17 +0530

    Update convertc2s.js

--- prefix/economy/convertc2s.js
+++ prefix/economy/convertc2s.js
@@ -1,8 +1,8 @@
 module.exports = {
     name: "convertc2s",
     type: "messageCreate",
-    code:`$if[$getVar[cookie;$authorID;0]>=100;$setVar[cookie;$authorID;$sub[$getVar[cookie;$authorID];100]]
-$setVar[supercookie;$authorID;$sum[1;$getVar[supercookie;$authorID]]];Need 100 cookie to convert]`,
+    code:`$if[$getVar[cookie;$authorID;0]>=200;$setVar[cookie;$authorID;$sub[$getVar[cookie;$authorID];200]]
+$setVar[supercookie;$authorID;$sum[1;$getVar[supercookie;$authorID]]];Need 200 cookie to convert]`,
     unprefixed: true
 
 }
