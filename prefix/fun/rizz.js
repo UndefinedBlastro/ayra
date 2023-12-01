bf7ad54832394f515845d9b78181fb2354bd7374
UndefinedBlastro <149256010+UndefinedBlastro@users.noreply.github.com>
2023-12-01 21:46:43 +0530

    Create rizz.js

---
+++ prefix/fun/rizz.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: "rizz",
+    type: "messageCreate",
+    code:`$addField[Rizz <:N_Heart:995707668469661776>;$function[$httpRequest[https://api.popcat.xyz/pickuplines;Get;l]] $env[l;pickupline]]$footer[$guildName[$guildID] | $timestamp;$guildIcon[$guildID]] $color[ca1bbf]$defer
+    `,
+    unprefixed: false
+
+}
