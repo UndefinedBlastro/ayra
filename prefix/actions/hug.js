8807d1aff2c1e321857f15d3ebba847869826f81
UndefinedBlastro <149256010+UndefinedBlastro@users.noreply.github.com>
2024-01-04 21:35:29 +0530

    Update hug.js
    re did the code to be more stable

--- prefix/actions/hug.js
+++ prefix/actions/hug.js
@@ -1,9 +1,11 @@
 module.exports = {
     name: "hug",
     type: "messageCreate",
-    code:`$if[$mentioned[0]!=;$arrayLoad[words;|;https://media.tenor.com/kCZjTqCKiggAAAAC/hug.gif
-    |https://media.tenor.com/MD7_IG4CUVAAAAAC/bree-time.gif|https://media.tenor.com/Qe6rYPM-FMkAAAAC/prsk-pjsekai.gif|https://media.tenor.com/wUQH5CF2DJ4AAAAC/horimiya-hug-anime.gif|https://media.tenor.com/IvZMcBk26R8AAAAC/hug-anime.gif|https://media.tenor.com/DlzqcBxAZEcAAAAC/hug-love.gif]
-    $title[$userDisplayName[$mentioned[0]], $userDisplayName[$authorID] hugs you] $image[$arrayAt[words;$math[$floor[$randomNumber[1;$arrayLength[words]]]]]]$color[ca1bbf];Please tag a user]`,
+    code:`$if[$mentioned[0]!=;$if[$mentioned[0]== $authorID;$title[$userDisplayName[$authorID] hugs $userDisplayName[$mentioned[0]] Awwe!] 
+    $image[$function[$httpRequest[https://api.lynnux.xyz/roleplay/hug.json;get]] $httpResult[embed;image;url]]
+    $color[Random];$title[I'll hug you instead $userDisplayName[$authorID] ] 
+    $image[$function[$httpRequest[https://api.lynnux.xyz/roleplay/hug.json;get]] $httpResult[embed;image;url]]
+    $color[Random]];Please tag a user]`,
     unprefixed: false
 
 }
