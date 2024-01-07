5ad069bc6af001901cedcd09cbf73ea9ac992008
UndefinedBlastro <149256010+UndefinedBlastro@users.noreply.github.com>
2024-01-07 15:52:18 +0530

    Update blush.js

--- prefix/actions/blush.js
+++ prefix/actions/blush.js
@@ -1,11 +1,9 @@
 module.exports = {
     name: "blush",
     type: "messageCreate",
-    code:`$if[$mentioned[0]!=;$if[$mentioned[0]!= $authorID;$title[$userDisplayName[$mentioned[0]] made $userDisplayName[$authorID] blush!] 
+    code:`$if[$mentioned[0]!=;$title[$userDisplayName[$mentioned[0]] made $userDisplayName[$authorID] blush!] 
     $image[$function[$httpRequest[https://api.lynnux.xyz/roleplay/blush.json;get]] $httpResult[embed;image;url]]
     $color[Random];$title[$userDisplayName[$authorID] is blushing! ] 
-    $image[$function[$httpRequest[https://api.lynnux.xyz/roleplay/blush.json;get]] $httpResult[embed;image;url]]
-    $color[Random]];$title[$userDisplayName[$authorID] is blushing! ] 
     $image[$function[$httpRequest[https://api.lynnux.xyz/roleplay/blush.json;get]] $httpResult[embed;image;url]]
     $color[Random]]`,
     unprefixed: false
