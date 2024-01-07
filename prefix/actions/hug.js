1f44748481a37147d9a85cd34bcba54a99904a5c
UndefinedBlastro <149256010+UndefinedBlastro@users.noreply.github.com>
2024-01-07 15:49:02 +0530

    Update hug.js
    Fixed the gif

--- prefix/actions/hug.js
+++ prefix/actions/hug.js
@@ -1,9 +1,9 @@
 module.exports = {
     name: "hug",
     type: "messageCreate",
-    code:`$if[$mentioned[0]!=;$if[$mentioned[0]== $authorID;$title[$userDisplayName[$authorID] hugs $userDisplayName[$mentioned[0]] Awwe!] 
+    code:`$if[$mentioned[0]!=;$if[$mentioned[0]==$authorID;$title[I'll hug you instead $userDisplayName[$authorID]] 
     $image[$function[$httpRequest[https://api.lynnux.xyz/roleplay/hug.json;get]] $httpResult[embed;image;url]]
-    $color[Random];$title[I'll hug you instead $userDisplayName[$authorID] ] 
+    $color[Random];$title[$userDisplayName[$authorID] hugs $userDisplayName[$mentioned[0]] Awwe!] 
     $image[$function[$httpRequest[https://api.lynnux.xyz/roleplay/hug.json;get]] $httpResult[embed;image;url]]
     $color[Random]];Please tag a user]`,
     unprefixed: false
