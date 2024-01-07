63b07b4abd908be05c3cd1a7c0193b8d50d597e6
UndefinedBlastro <149256010+UndefinedBlastro@users.noreply.github.com>
2024-01-07 16:06:59 +0530

    Update slap.js

--- prefix/actions/slap.js
+++ prefix/actions/slap.js
@@ -1,9 +1,9 @@
 module.exports = {
     name: "slap",
     type: "messageCreate",
-    code:`$if[$mentioned[0]!=;$if[$mentioned[0]== $authorID;$title[$userDisplayName[$authorID] slaps $userDisplayName[$mentioned[0]] RIP!] 
+    code:`$if[$mentioned[0]!=;$if[$mentioned[0]==$authorID;I would never do that!!;$title[$userDisplayName[$authorID] slaps $userDisplayName[$mentioned[0]] RIP!] 
     $image[$function[$httpRequest[https://api.lynnux.xyz/roleplay/slap.json;get]] $httpResult[embed;image;url]]
-    $color[Random];Why would you do that?];Please tag a user]`,
+    $color[Random]];Please tag a user]`,
     unprefixed: false
 
 }
