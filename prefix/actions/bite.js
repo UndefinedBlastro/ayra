d07e2e99d9568e45c6c35a402005d83347d920e9
UndefinedBlastro <149256010+UndefinedBlastro@users.noreply.github.com>
2024-01-07 15:50:29 +0530

    Update bite.js
    fixed the self bite command

--- prefix/actions/bite.js
+++ prefix/actions/bite.js
@@ -1,9 +1,9 @@
 module.exports = {
     name: "bite",
     type: "messageCreate",
-    code:`$if[$mentioned[0]!=;$if[$mentioned[0]== $authorID;$title[$userDisplayName[$authorID] bit $userDisplayName[$mentioned[0]] oof!] 
+    code:`$if[$mentioned[0]!=;$if[$mentioned[0]==$authorID;How do you even do that?;$title[$userDisplayName[$authorID] bit $userDisplayName[$mentioned[0]] oof!] 
     $image[$function[$httpRequest[https://api.lynnux.xyz/roleplay/bite.json;get]] $httpResult[embed;image;url]]
-    $color[Random];How do you even do that?];Please tag a user]`,
+    $color[Random]];Please tag a user]`,
     unprefixed: false
 
 }
