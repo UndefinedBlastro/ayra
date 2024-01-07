e82aef5605dc7e90cd5b96219e9e680be58b2c61
UndefinedBlastro <149256010+UndefinedBlastro@users.noreply.github.com>
2024-01-07 15:54:18 +0530

    Update bonk.js

--- prefix/actions/bonk.js
+++ prefix/actions/bonk.js
@@ -1,9 +1,9 @@
 module.exports = {
     name: "bonk",
     type: "messageCreate",
-    code:`$if[$mentioned[0]!=;$if[$mentioned[0]!= $authorID;$title[$userDisplayName[$authorID] bonked $userDisplayName[$mentioned[0]]!] 
+    code:`$if[$mentioned[0]!=;$if[$mentioned[0]==$authorID;How do you even bonk yourself?;$title[$userDisplayName[$authorID] bonked $userDisplayName[$mentioned[0]]!] 
     $image[$function[$httpRequest[https://api.lynnux.xyz/roleplay/bonk.json;get]] $httpResult[embed;image;url]]
-    $color[Random];What?];Please tag a user]`,
+    $color[Random]];Please tag a user]`,
     unprefixed: false
 
 }
