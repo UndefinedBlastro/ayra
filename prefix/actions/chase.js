5118cddec8983fab811c5adc53bf097dfd7de8a1
UndefinedBlastro <149256010+UndefinedBlastro@users.noreply.github.com>
2024-01-07 15:59:25 +0530

    Update chase.js

--- prefix/actions/chase.js
+++ prefix/actions/chase.js
@@ -1,9 +1,9 @@
 module.exports = {
     name: "chase",
     type: "messageCreate",
-    code:`$if[$mentioned[0]!=;$if[$mentioned[0]!= $authorID;$title[$userDisplayName[$authorID] is chasing $userDisplayName[$mentioned[0]]!] 
+    code:`$if[$mentioned[0]!=;$if[$mentioned[0]==$authorID;How do you chase yourself?;$title[$userDisplayName[$authorID] is chasing $userDisplayName[$mentioned[0]]!] 
     $image[$function[$httpRequest[https://api.lynnux.xyz/roleplay/chase.json;get]] $httpResult[embed;image;url]]
-    $color[Random];how do yu chase yourself?];Please tag a user]`,
+    $color[Random]];Please tag a user]`,
     unprefixed: false
 
 }
