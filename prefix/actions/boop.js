0d6667268ff4a0de84c64b2614aa32d0c31aa2e2
UndefinedBlastro <149256010+UndefinedBlastro@users.noreply.github.com>
2024-01-07 15:55:46 +0530

    Update boop.js

--- prefix/actions/boop.js
+++ prefix/actions/boop.js
@@ -1,9 +1,9 @@
 module.exports = {
     name: "boop",
     type: "messageCreate",
-    code:`$if[$mentioned[0]!=;$if[$mentioned[0]!= $authorID;$title[$userDisplayName[$authorID] booped $userDisplayName[$mentioned[0]]!] 
+    code:`$if[$mentioned[0]!=;$if[$mentioned[0]==$authorID;You can't boop yourself Silly!;$title[$userDisplayName[$authorID] booped $userDisplayName[$mentioned[0]]!] 
     $image[$function[$httpRequest[https://api.lynnux.xyz/roleplay/boop.json;get]] $httpResult[embed;image;url]]
-    $color[Random];How do you do that?];Please tag a user]`,
+    $color[Random]];Please tag a user]`,
     unprefixed: false
 
 }
